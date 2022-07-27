f3e98a0c5b411bd09701de2f0621abc53355568b
yinmsk <galaxy220111@gmail.com>
2022-07-27 12:05:51 +0900

    no message

--- baekjoon/02_1110.py
+++ baekjoon/02_1110.py
@@ -1,3 +1,17 @@
 "더하기 싸이클"
 "https://www.acmicpc.net/problem/1110"
 
+N = int(input()) 
+num = N           
+count = 0         
+ 
+while True:
+    a = num//10
+    b = num %10
+    c = (a+b)%10
+    num = (b*10) + c
+    count += 1
+    if(num == N):
+        break
+ 
+print(count)
