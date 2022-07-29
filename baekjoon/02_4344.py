8e0a7560472b4d39ae748e23f1ed9431a3ba47c6
yinmsk <galaxy220111@gmail.com>
2022-07-29 10:22:10 +0900

    no message

--- baekjoon/02_4344.py
+++ baekjoon/02_4344.py
@@ -1,2 +1,15 @@
 "평균은 넘겠지"
 "https://www.acmicpc.net/problem/4344"
+
+
+n = int(input())
+
+for _ in range(n):
+    nums = list(map(int, input().split()))
+    avg = sum(nums[1:])/nums[0]
+    cnt = 0
+    for score in nums[1:]:
+        if score > avg:
+            cnt += 1
+    rate = cnt/nums[0] *100
+    print(f'{rate:.3f}%')
